9dde1397d15e6aaec1f7a763be69292b005401ce
Sławomir Mucha <mucha.slawomir@gmail.com>
2018-12-13 17:03:58 +0100

    Extract targeting policies

--- src/Event/EventDistributor.ts
+++ src/Event/EventDistributor.ts
@@ -1,18 +1,23 @@
 import {Event} from "../Interfaces/Event";
 import {Recipient} from "../Recipient/Recipient";
+import {RecipientTargetingPolicy} from "../Interfaces/RecipientTargetingPolicy";
+import {FollowedTopicsPolicy} from "./TargetingPolicies/FollowedTopicsPolicy";
 
 
 // Responsibility: targeting recipients
 // Analyzed properties:
 // - interest expressed in following given topic,
 // TODO: - distance between event and recipient location,
-
 export class EventDistributor {
 
     private readonly events: Event[];
+    private readonly targetingPolicies: RecipientTargetingPolicy[];
 
     constructor(events: Event[]) {
         this.events = events;
+        this.targetingPolicies = [
+            new FollowedTopicsPolicy()
+        ];
     }
 
     public distribute(recipients: Recipient[]): Map<string, Event[]> {
@@ -32,14 +37,12 @@ export class EventDistributor {
             }
 
         }
-
         return recipientEvents;
     }
 
     private shouldKnowAbout(recipient: Recipient, event: Event): boolean {
-        const followedTopics = new Set(recipient.followedTopics);
-        for (const topic of event.topics) {
-            if (followedTopics.has(topic)) {
+        for (const policy of this.targetingPolicies) {
+            if (policy.shouldKnowAbout(recipient, event)) {
                 return true;
             }
         }
---
+++ src/Interfaces/RecipientTargetingPolicy.ts
@@ -0,0 +1,6 @@
+import {Recipient} from "../Recipient/Recipient";
+import {Event} from "./Event";
+
+export interface RecipientTargetingPolicy {
+    shouldKnowAbout(recipient: Recipient, event: Event);
+}
