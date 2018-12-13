11e56fbf0906cfeb17f3ea773c23c5023e13e623
Sławomir Mucha <mucha.slawomir@gmail.com>
2018-12-13 16:36:36 +0100

    Add app.ts

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+
+createConnection().then(async connection => {
+
+
+}).catch(error => console.log(error));
+
+
+
+const run = () => setTimeout(() => run(), 1000);
+
+run();
