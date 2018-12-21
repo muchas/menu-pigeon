b538a62b73003766fa66ed528ef02662c0b83e2a
Sławomir Mucha <mucha.slawomir@gmail.com>
2018-12-21 17:01:23 +0100

    Add logging

--- src/Recipient/RecipientDeletedConsumer.ts
+++ src/Recipient/RecipientDeletedConsumer.ts
@@ -2,6 +2,7 @@ import { Consumer, Job } from "queue";
 import { RecipientRepository } from "./RecipientRepository";
 import { RecipientDeleted } from "queue/lib/Messages/RecipientDeleted";
 import { injectable } from "inversify";
+import * as winston from "winston";
 
 @injectable()
 export class RecipientDeletedConsumer implements Consumer {
@@ -9,6 +10,14 @@ export class RecipientDeletedConsumer implements Consumer {
     public constructor(private readonly recipientRepository: RecipientRepository) {}
 
     public async consume(job: Job<RecipientDeleted>) {
+        winston.info("Consumption of recipient deleted started", {
+            recipient_id: job.message.id,
+        });
+
         await this.recipientRepository.remove(job.message.id);
+
+        winston.info("Consumption of recipient deleted finished", {
+            recipient_id: job.message.id,
+        });
     }
 }
