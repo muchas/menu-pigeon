0aba46d21bbfe2c5e5f5c8b7f4f62c6a714f7cf1
Sławomir Mucha <mucha.slawomir@gmail.com>
2019-01-23 20:56:27 +0100

    Fix invalid or (#34)
    * Fix invalid or
    
    * Fix lint

--- test/Integration/PushNotificationRepositoryTest.ts
+++ test/Integration/PushNotificationRepositoryTest.ts
@@ -6,6 +6,7 @@ import { PushNotificationRepository } from "../../src/PushNotification/PushNotif
 import { Message } from "../../src/Entity/Message";
 import { PushNotification } from "../../src/Entity/PushNotification";
 import { PushNotificationStatus } from "../../src/PushNotification/PushNotificationReceipt";
+import * as moment from "moment-timezone";
 
 const createMessage = (recipientId: string, topics: string[] = [], eventType= "default") => {
     const message = new Message();
@@ -13,7 +14,7 @@ const createMessage = (recipientId: string, topics: string[] = [], eventType= "d
     message.title = "Hej John, lunch dnia!";
     message.body = "Sprawdź szczegóły";
     message.priority = "default";
-    message.expirationTime = new Date();
+    message.expirationTime = moment().add(2, "day").toDate();
     message.setTopics(topics);
     message.setEventType(eventType);
     return message;
@@ -22,12 +23,14 @@ const createMessage = (recipientId: string, topics: string[] = [], eventType= "d
 const createPushNotification = (message: Message,
                                 token: string,
                                 status: number,
+                                lockedUntil: Date = null,
                                 receiptId?: string): PushNotification => {
     const notification = new PushNotification();
     notification.status = status;
     notification.pushToken = token;
     notification.message = message;
     notification.receiptId = receiptId;
+    notification.lockedUntil = lockedUntil;
     return notification;
 };
 
@@ -61,5 +64,22 @@ describe("RecipientRepository test", () => {
         // then
         expect(notifications).to.be.lengthOf(2);
         expect(notificationsAfterLock).to.be.lengthOf(0);
+    });
+
+    it("should not include sent notifications which are not locked anymore", async () => {
+        // given
+        const yesterday = moment().subtract(1, "day");
+        const message = createMessage("recipient#1", ["topic-#1"]);
+        const notification1 = createPushNotification(message, "12412", PushNotificationStatus.SCHEDULED);
+        const notification2 = createPushNotification(message, "12512", PushNotificationStatus.SENT, yesterday.toDate());
+        message.pushNotifications = [notification1, notification2];
+
+        await notificationRepository.storeMessagesToSend([message]);
+
+        // when
+        const notifications = await notificationRepository.fetchForSending();
+
+        // then
+        expect(notifications).to.be.lengthOf(1);
     });
 });
