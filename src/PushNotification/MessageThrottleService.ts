c6b88fef402c0a67aba14d212c5b04eff99992af
Sławomir Mucha <mucha.slawomir@gmail.com>
2019-02-04 14:18:06 +0100

    Fix frequency rule, prevent sending notifications in case of lack of … (#40)
    * Fix frequency rule, prevent sending notifications in case of lack of receiptId
    
    * CR suggestions

--- src/PushNotification/MessageThrottleService.ts
+++ src/PushNotification/MessageThrottleService.ts
@@ -1,6 +1,5 @@
 import { Recipient } from "../Recipient/Recipient";
 import { Message } from "../Entity/Message";
-import { LUNCH_EVENT_TYPE } from "../Publication/constants";
 import { NotificationLevel } from "queue/lib/Messages/Recipient";
 import * as moment from "moment-timezone";
 import { DurationInputArg2 } from "moment-timezone";
@@ -21,6 +20,26 @@ class NeverMessageRule implements MessageThrottleRule {
 class FrequencyMessageRule implements MessageThrottleRule {
     public constructor(
         private readonly notificationLevel: NotificationLevel,
+        private readonly granularity: moment.unitOfTime.StartOf,
+    ) {}
+
+    public filter(recipient: Recipient, messages: Message[]): Message[] {
+        if (
+            recipient.preferences.level === this.notificationLevel &&
+            recipient.lastNotificationTime
+        ) {
+            if (!moment().isSame(recipient.lastNotificationTime, this.granularity)) {
+                return messages;
+            }
+            return [];
+        }
+        return messages;
+    }
+}
+
+class CycleMessageRule implements MessageThrottleRule {
+    public constructor(
+        private readonly notificationLevel: NotificationLevel,
         private readonly unit: DurationInputArg2,
     ) {}
 
@@ -29,29 +48,21 @@ class FrequencyMessageRule implements MessageThrottleRule {
             recipient.preferences.level === this.notificationLevel &&
             recipient.lastNotificationTime
         ) {
-            const weekAgo = moment().subtract("1", this.unit);
-            if (recipient.lastNotificationTime <= weekAgo) {
-                return messages.length > 0 ? [messages[0]] : messages;
-            } else {
-                return [];
+            const cycleStart = moment().subtract("1", this.unit);
+            if (recipient.lastNotificationTime <= cycleStart) {
+                return messages;
             }
+            return [];
         }
         return messages;
     }
 }
 
-class LunchMessageRule implements MessageThrottleRule {
+class LimitRule implements MessageThrottleRule {
+    public constructor(private readonly limit: number) {}
+
     public filter(recipient: Recipient, messages: Message[]): Message[] {
-        let lunchEncountered = false;
-        return messages.filter(message => {
-            if (message.eventType === LUNCH_EVENT_TYPE) {
-                if (lunchEncountered) {
-                    return false;
-                }
-                lunchEncountered = true;
-            }
-            return true;
-        });
+        return messages.slice(0, this.limit);
     }
 }
 
@@ -63,8 +74,8 @@ export class MessageThrottleService {
             new NeverMessageRule(),
             new FrequencyMessageRule(NotificationLevel.Seldom, "week"),
             new FrequencyMessageRule(NotificationLevel.Daily, "day"),
-            new FrequencyMessageRule(NotificationLevel.Often, "hour"),
-            new LunchMessageRule(),
+            new CycleMessageRule(NotificationLevel.Often, "hour"),
+            new LimitRule(1),
         ];
     }
 
