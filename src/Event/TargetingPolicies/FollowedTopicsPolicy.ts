a886240405935229d90a49b0660de5cb9fb06eea
Sławomir Mucha <mucha.slawomir@gmail.com>
2019-02-23 17:36:10 +0100

    Add ready time to event distribution policy (#46)

--- src/Event/TargetingPolicies/FollowedTopicsPolicy.ts
+++ src/Event/TargetingPolicies/FollowedTopicsPolicy.ts
@@ -6,7 +6,10 @@ export class FollowedTopicsPolicy implements RecipientTargetingPolicy {
     public shouldKnowAbout(recipient: Recipient, event: Event): boolean {
         for (const topic of event.topics) {
             const followedAt = recipient.followedTopics.get(topic);
-            if (followedAt !== undefined && followedAt.isBefore(event.registeredAt)) {
+            if (
+                followedAt !== undefined &&
+                (followedAt.isBefore(event.readyTime) || followedAt.isBefore(event.registeredAt))
+            ) {
                 return true;
             }
         }
--- test/Unit/Event/EventDistributorTest.ts
+++ test/Unit/Event/EventDistributorTest.ts
@@ -15,18 +15,21 @@ describe("EventDistributor", () => {
             id: "kjcblk10-9eknjsal",
             eventType: "lunch-offer",
             topics: ["topic-1", "topic-2"],
+            readyTime: now,
             registeredAt: now,
         };
         const event2 = {
             id: "i1uj4-n1h24-cbm19",
             eventType: "lunch-offer",
             topics: ["topic-2"],
+            readyTime: now,
             registeredAt: now,
         };
         const event3 = {
             id: "iu12h4iu-1kj24-j124",
             eventType: "lunch-offer",
             topics: ["topic-1"],
+            readyTime: now,
             registeredAt: now,
         };
         const events = [event1 as Event, event2 as Event, event3 as Event];
@@ -50,7 +53,7 @@ describe("EventDistributor", () => {
         expect(relevantEvents).to.deep.equal([event1, event3]);
     });
 
-    it("skip events that registered before follow action", async () => {
+    it("skip events that were followed after ready time", async () => {
         // given
         const now = moment();
         const justAfterNow = moment(now).add(1, "second");
@@ -58,22 +61,18 @@ describe("EventDistributor", () => {
         const event1 = {
             id: "kjcblk10-9eknjsal",
             eventType: "lunch-offer",
-            topics: ["topic-1", "topic-2"],
+            topics: ["topic-1"],
+            readyTime: now,
             registeredAt: now,
         };
         const event2 = {
-            id: "i1uj4-n1h24-cbm19",
-            eventType: "lunch-offer",
-            topics: ["topic-2"],
-            registeredAt: now,
-        };
-        const event3 = {
             id: "iu12h4iu-1kj24-j124",
             eventType: "lunch-offer",
             topics: ["topic-1"],
+            readyTime: now,
             registeredAt: now,
         };
-        const events = [event1 as Event, event2 as Event, event3 as Event];
+        const events = [event1 as Event, event2 as Event];
 
         const distributor = new EventDistributor();
         const recipient = new Recipient(
@@ -91,5 +90,45 @@ describe("EventDistributor", () => {
 
         // then
         expect(relevantEvents).to.be.lengthOf(0);
+    });
+
+    it("take events that were followed before registering", async () => {
+        // given
+        const now = moment();
+        const justBeforeNow = moment(now).subtract(1, "second");
+        const justAfterNow = moment(now).add(1, "second");
+
+        const event1 = {
+            id: "kjcblk10-9eknjsal",
+            eventType: "lunch-offer",
+            topics: ["topic-1"],
+            readyTime: justBeforeNow,
+            registeredAt: justAfterNow,
+        };
+        const event2 = {
+            id: "iu12h4iu-1kj24-j124",
+            eventType: "lunch-offer",
+            topics: ["topic-1"],
+            readyTime: justBeforeNow,
+            registeredAt: justAfterNow,
+        };
+        const events = [event1 as Event, event2 as Event];
+
+        const distributor = new EventDistributor();
+        const recipient = new Recipient(
+            "recipient#1",
+            "John",
+            [],
+            new RecipientPreferences(9, 0, NotificationLevel.Daily),
+            new Set(),
+            new Map(),
+            new Map([["topic-1", now]]),
+        );
+
+        // when
+        const relevantEvents = distributor.filterRelevantFor(recipient, events);
+
+        // then
+        expect(relevantEvents).to.be.lengthOf(2);
     });
 });
