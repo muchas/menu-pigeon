0ee8e5a21155aad51dad2d2d3d406ef5354da6f9
Sławomir Mucha <mucha.slawomir@gmail.com>
2019-02-28 12:30:33 +0100

    Prevent publication multiple processing (#48)
    * Prevent publication multiple processing
    
    * Remove suffix

---
+++ src/Publication/PersistedPublicationRepository.ts
@@ -0,0 +1,34 @@
+import { injectable } from "inversify";
+import { PersistedPublication } from "queue/lib/Messages/PersistedPublication";
+import Mongo from "../Mongo";
+import { Collection } from "mongodb";
+
+@injectable()
+export class PersistedPublicationRepository {
+    private static readonly COLLECTION_NAME: string = "persisted_publication_ids";
+
+    public constructor(private readonly mongo: Mongo) {
+        //
+    }
+
+    public async add(publication: PersistedPublication): Promise<boolean> {
+        const result = await this.collection().updateOne(
+            {
+                id: publication.id,
+            },
+            {
+                $set: {
+                    id: publication.id,
+                },
+            },
+            {
+                upsert: true,
+            },
+        );
+        return result.upsertedCount === 1;
+    }
+
+    private collection(): Collection {
+        return this.mongo.db.collection(PersistedPublicationRepository.COLLECTION_NAME);
+    }
+}
---
+++ test/Integration/PersistedPublicationRepositoryTest.ts
@@ -0,0 +1,65 @@
+import { expect } from "chai";
+import "reflect-metadata";
+import { Container } from "inversify";
+import { setupWithMongo, tearDownWithMongo } from "../utils";
+import { PersistedPublicationRepository } from "../../src/Publication/PersistedPublicationRepository";
+import { PersistedPublication } from "queue/lib/Messages/PersistedPublication";
+import * as moment from "moment-timezone";
+
+describe("PersistedPublicationRepository", () => {
+    let container: Container;
+    let publicationRepository: PersistedPublicationRepository;
+    let persistedPublication: PersistedPublication;
+
+    beforeEach(async () => {
+        container = await setupWithMongo();
+
+        publicationRepository = container.get<PersistedPublicationRepository>(
+            PersistedPublicationRepository,
+        );
+    });
+
+    afterEach(async () => {
+        await tearDownWithMongo(container);
+    });
+
+    it("should insert publications on add @slow", async () => {
+        // given
+        const now = moment().toDate();
+        persistedPublication = new PersistedPublication(
+            521,
+            "n4nx83mwi",
+            "Awesome Biz",
+            "awesome-biz",
+            [],
+            now,
+        );
+
+        // when
+        const inserted = await publicationRepository.add(persistedPublication);
+
+        // then
+        expect(inserted).to.be.true;
+    });
+
+    it("should not insert existing publications on add @slow", async () => {
+        // given
+        const now = moment().toDate();
+        persistedPublication = new PersistedPublication(
+            521,
+            "n4nx83mwi",
+            "Awesome Biz",
+            "awesome-biz",
+            [],
+            now,
+        );
+
+        await publicationRepository.add(persistedPublication);
+
+        // when
+        const inserted = await publicationRepository.add(persistedPublication);
+
+        // then
+        expect(inserted).to.be.false;
+    });
+});
