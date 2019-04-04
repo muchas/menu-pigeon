afa38b2a4546b8e48d608a7569984f616fd75af1
Sławomir Mucha <mucha.slawomir@gmail.com>
2019-04-04 20:20:37 +0200

    Add sync of old device holders

--- src/Interfaces/RecipientRepository.ts
+++ src/Interfaces/RecipientRepository.ts
@@ -5,8 +5,10 @@ import { injectable } from "inversify";
 export abstract class RecipientRepository {
     public abstract async findAll(): Promise<Recipient[]>;
     public abstract async findByDevice(pushToken: string): Promise<Recipient[]>;
+    public abstract async findByDevices(pushTokens: string[]): Promise<Recipient[]>;
     public abstract async findOne(id: string): Promise<Recipient | undefined>;
     public abstract async addMany(recipients: Recipient[]): Promise<void>;
     public abstract async add(recipient: Recipient): Promise<void>;
     public abstract async remove(id: string): Promise<void>;
+    public abstract async removeMany(ids: string[]): Promise<void>;
 }
